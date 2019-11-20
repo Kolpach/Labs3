98e169764b4f8773e0e24915467a8c1ba40fed03
Max Kushchenko <AkForCorona@gmail.com>
2019-11-21 02:42:17 +0700

    delete some random appeared files

--- YAP_Lab7(6.1, 6.2, 6.3)/YAP_Lab6/YAP_Lab6/TimeTable.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <vector>
-#include <map>
-void nextYear();
-std::vector<int> nextMonth(std::map<int, std::vector<std::string>> &timetable);
-void addLesson(std::map<int, std::vector<std::string>> &timetable, int day, std::string title);
-void outPut(std::map<int, std::vector<std::string>> &timetable, int day);
-void show_all(std::map<int, std::vector<std::string>> &timetable);
